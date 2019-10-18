23548737fb116186e0f1de8bdc6013aa2845d7a0
xuan <mxuanp@protonmail.com>
2019-10-18 15:24:35 +0800

    update search.py file, creaete conf.py file

---
+++ conf.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+mysql_host = 'localhost'
+mysql_user = 'ayako'
+mysql_password = '123321'
+mysql_db = 'site'
--- search.py
+++ search.py
@@ -5,14 +5,18 @@ import sys
 import requests
 import random
 import re
+import pymysql
+import _thread
+import time
 import html_str_util
+import conf
 from pool import Pool
 from bs4 import BeautifulSoup
 
 
 #global variabal
 search_url = 'https://www.xbiquge6.com/search.php'
-
+fictions = []
 #search fiction in https://www.xbiquge6.com/search.php
 def search(filter):
     html = get_html(search_url+'?keyword='+filter)
@@ -23,22 +27,65 @@ def search(filter):
         result_json = result_json + get_item(div) + ','
     result_json = result_json + '}'
     print(result_json)
+    write_db(fictions)
 #parse the result item,which has fiction's info
 def get_item(item):
+    name = ''
+    image = ''
+    intro = ''
+    url = ''
+    author = ''
+    num = 0
+    create_date = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+    updating = 0
     item_str = '[' 
     for child in item.descendants:
         if child.name == 'img':
             item_str = item_str+'\"image\":\"'+child.get('src')+'\",'
+            image = child.get('src')
         if child.name == 'a' and child.has_attr('title'):
-            item_str = item_str+'\"url\":\"' + child.get('href')+'\",'+'\"title\":\"'+child.get('title')+'\",'
+            item_str = item_str+'\"url\":\"' + child.get('href')+'\",'+'\"name\":\"'+child.get('title')+'\",'
+            url = child.get('href')
+            name = child.get('title')
         if child.name == 'p' and child.has_attr('class') and 'result-game-item-desc' in child['class']:
             item_str = item_str + '\"intro\":\"' + child.get_text() + '\",'
+            intro = child.get_text()
         if child.name == 'p' and child.has_attr('class') and 'result-game-item-info-tag' in child['class']:
             item_str = item_str + '\"author\":\"' + trim(child.get_text()) + '\"'
+            author = trim(child.get_text())
             break
     item_str = item_str + ']'
+    fictions.append((name,intro,url,image,author,num,create_date,updating))
     return item_str
+#create a new thread to write data
+def write_db_task(fictions_list):
+    try:
+        _thread.start_new_thread(write_db, (fictions_list))
+    except:
+        print('\"status\":\"500\",\"msg\":\"new thread error\"')
+#write data to database
+def write_db(fictions_list):
+    db = pymysql.connect(conf.mysql_host, conf.mysql_user, conf.mysql_password, conf.mysql_db)
+    insert_sql = "insert into fiction(name,intro,url,image,author,num,create_date,updating) values(%s,%s,%s,%s,%s,%s,%s,%s);"
+    select_sql = "select count(url) from fiction where url = %s;"
+    cursor = db.cursor()
+    try:
+        for fiction in fictions_list:
+            cursor.execute(select_sql,(pymysql.escape_string(fiction[2])))
+            result = cursor.fetchone()
+            if result[0] == 0:
+                cursor.execute(insert_sql,(pymysql.escape_string(fiction[0]),pymysql.escape_string(fiction[1]),pymysql.escape_string(fiction[2]),pymysql.escape_string(fiction[3]),pymysql.escape_string(fiction[4]),fiction[5],pymysql.escape_string(fiction[6]),fiction[7]))
+        db.commit()
+    except:
+        db.rollback()
+        print('[\"status\":\"500\",\"msg\":\"database error\"]')
+    cursor.close()
+    db.close()
 
+
+
+
+#去除多余的空行
 def trim(string):
     string = string.replace('\n', '')
     string = string.replace('\r', '')

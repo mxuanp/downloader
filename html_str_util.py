e0d365ff482169467aca1501fd3cde4ba3c53fbd
xuan <mxuanp@protonmail.com>
2019-11-07 14:12:24 +0800

    update html_str_util.py

--- html_str_util.py
+++ html_str_util.py
@@ -4,13 +4,15 @@
 import re
 
 def filter_tags(htmlstr):
+    htmlstr = htmlstr.replace("&nbsp;"," ")
+    htmlstr = htmlstr.replace("<br />","\n")
+    '''
     blank_line=re.compile('\n+')
     s=blank_line.sub('',htmlstr)
     blank_line_l=re.compile('\n')
     s=blank_line_l.sub('',s)
     blank_kon=re.compile('\t')
     s=blank_kon.sub('',s)
-    '''
     blank_one=re.compile('\r\n')
     s=blank_one.sub('',s)
     blank_two=re.compile('\r')
@@ -18,4 +20,4 @@ def filter_tags(htmlstr):
     blank_three=re.compile(' ')
     s=blank_three.sub('',s)
     '''
-    return s
+    return htmlstr
